eb18a210a54a154ade4021a0a7c8b363effdef02
ji.dong@hotmail.co.uk <ji.dong@hotmail.co.uk>
2016-12-09 14:45:33 +0000

    move irq files to customer package, and disable generation from BSP

--- software/nios_dev/main.c
+++ software/nios_dev/main.c
@@ -7,8 +7,34 @@
 
 #include <stdio.h>
 
+#include <sys/alt_alarm.h>
+#include <priv/alt_busy_sleep.h>
+
+static alt_alarm alarm;
+static int mainCounter = 0;
+static int timerCounter = 0;
+
+alt_u32 TimerHandler( void * context )
+{
+    /* Increment the kernel tick. */
+    printf("Hello from Timer  %d!\n",  timerCounter++);
+
+    return alt_ticks_per_second();
+}
+
+
 int main()
 {
     printf("Hello from Main!\n");
+
+    alt_alarm_start(&alarm, alt_ticks_per_second(), TimerHandler, NULL);
+
+    while(1)
+    {
+        alt_busy_sleep(1000000);
+        printf("Hello from Main  %d!\n",  mainCounter++);
+    }
+
+
     return 0;
 }
